9510bf5ef02036ef8869e4bc00c9d66f33cd2252
gabbo3a <gamara.work@gmail.com>
2026-05-18 18:44:40 +0000

    feat(curry-howard): implement context data structure

---
+++ playground/prototypes/curry-howard/CurryHoward/Context.lean
@@ -0,0 +1,10 @@
+import Init.Data.Vector
+
+namespace CurryHoward.Context
+  abbrev Context (α : Type u) (n : Nat) := Vector α n
+
+  def Empty {α : Type u} : Context α 0 := ⟨#[], rfl⟩
+
+  def extend {α : Type u} {n : Nat} (x : α) (Γ : Context α n) : Context α (n + 1) :=
+    ⟨(x :: Γ.toList).toArray, by simp⟩
+end CurryHoward.Context
